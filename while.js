5995bfe29e724d5aca01726733b88deaaed04325
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 07:07:51 +0800

    even number and odd number unsing while loop

---
+++ while.js
@@ -0,0 +1,28 @@
+// let count=0;
+// while(count<20){
+    
+//     console.log("This is value is " +count);
+//     count++;
+// }
+
+// for(let i=0;i<20;i++){
+//     console.log("This is value is " +i)
+// }
+
+
+
+// even number useing while loop
+
+
+let i =2;
+while(i<10){
+    console.log("The even number is " +i);
+    i=i+2;
+}
+
+
+let count =1;
+while(count<20){
+    console.log("The odd number is " +count);
+    count=count+2;
+}
