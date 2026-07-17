db34f3e3ee1b9bd5dfcf270f6b0cabad01eb531b
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 05:54:50 +0800

    first commit

---
+++ loop.js
@@ -0,0 +1,19 @@
+// let conunt=0;
+// for conunt >10;
+// count++
+// console.log("do sumthine")
+
+
+
+// for(let count=0;count <10;count++){
+//     console.log("do sumthing")
+// }
+
+// for(let count=0;count <30;count++){
+//     console.log("Hello")
+// }
+
+
+for(let count=0;count <11;count++){
+    console.log("Hi")
+}
