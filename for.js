d71dead2e8fafc28c61de1ad357bffa72cecaf17
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 06:27:00 +0800

    evand and oddd numuber using for loop

---
+++ for.js
@@ -0,0 +1,19 @@
+// for(let i=0;i <3;i++){
+//     console.log("value of i : " +i)
+// }
+
+
+// for(let i=1;i<20;i=i+2){
+//     console.log("The odd value is " +i)
+// }
+
+
+
+
+// Even number
+
+// for(i=2;i<20;i=i+2){
+//     console.log("The even number is " +i)
+// }
+
+
