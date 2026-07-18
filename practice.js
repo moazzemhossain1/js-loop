e72662d4bfa7493b563d93f19b7733b932b86550
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 18:56:18 +0800

    all parctice today

---
+++ practice.js
@@ -0,0 +1,225 @@
+// 01
+
+// for(let i=1;i<=5;i++){
+//     console.log("Hello")
+// }
+
+// 02
+
+
+// for(let i=1;i<=10;i++ ){
+//     console.log(i)
+// }
+
+// 03 
+
+
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// 04
+
+
+
+// let i=1;
+// while(i<=7){
+//     console.log("Loop is Runing.");
+//     i++;
+// }
+
+
+// 05
+
+
+// for(i=2;i<=30;i=i+2){
+//     console.log("The even number is", i)
+
+// }
+
+
+// 06
+
+
+
+// sum=0;
+// for(let i =1;i<=20;i++){
+//     sum=sum+i;
+    
+// }
+// console.log("The tottle sum is:",sum)
+
+
+// 07 
+// let number=7
+
+// for(let i = 1; i<=10;i++){
+//     console.log(number , 'x' , i , '=' , i*number)
+// }
+
+
+//  08
+
+// let i=20;
+// while(i >=1){
+//     console.log(i);
+//     i--;
+// }
+
+
+//  09
+
+// let sum=0;
+// for(let i=2;i<=50;i=i+2){
+//     sum=sum+i;
+    
+// }
+// console.log("The tottle sum is : ", sum)
+
+
+// 10
+
+
+
+
+// for(let i=1;i<=50;i++){
+//     console.log(i)
+//     if(i>=30){
+//         break;
+
+//     }
+    
+// }
+
+
+
+//  12
+
+
+// for(let i =1;i<=20;i++){
+   
+//     if(i%4===0){
+//         continue;
+//     }
+//      console.log(i);
+// }
+
+
+
+// let i=1;
+// while(i<=20){
+    
+//     i++;
+//     if(i%0===0){
+//         continue;
+
+//     }
+//     console.log(i);
+    
+// }
+
+
+
+
+
+// for(let i=1; i<=100;i++){
+//     if(i%5==0){
+//         continue;
+//     }
+//     if(i>=40){
+//         break;
+        
+//     }
+//     console.log(i)
+    
+// }
+
+
+// let i=10;
+// while(i>=1){
+//     console.log(i)
+//     i--;
+// }
+
+
+
+// for(let i=2;i<=50;i=i+2){
+//     console.log(i)
+// }
+
+
+
+// for (let i =1;i<=29;i=i+2){
+//     console.log(i)
+// }
+
+
+
+
+// let sum=0;
+// for(let i=1;i<=100;i++){
+//     sum=sum+i;
+// }
+// console.log(sum)
+
+
+
+
+// let number=5;
+// for(let i=1;i<=10;i++){
+//     console.log(number , 'x' , i, '=' ,i*number)
+// }
+
+
+// let count=0;
+// for(let i=1;i<=100;i++){
+//     count++;
+    
+// }
+// console.log("Total numbers = ", count)
+
+
+
+// let factorial=1;
+// for(let i =1;i<=5;i++){
+//     factorial=factorial*i;
+// }
+// console.log("The factorial is: " ,factorial)
+
+// for(let i=5;i>=1; i--){
+//     console.log(i)
+
+// }
+
+
+// let i=1;
+// while(i<=50){
+//     if(i>=20){
+//         break;
+        
+//     }
+//     console.log(i);
+    
+//     i++;
+// }
+
+
+
+let numbers=[5,10,15,20,25,30];
+
+for(let i=0;i<numbers.length;i++){
+    if(numbers[i]===20){
+         console.log("Find This number :", numbers[i])
+        break;
+    }
+   
+}
