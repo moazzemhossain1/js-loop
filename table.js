66e535071a144dbaad198e38477db4f5e022a6a0
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 08:06:07 +0800

    sum and tavese

---
+++ table.js
@@ -0,0 +1,16 @@
+// let number=3;
+// for(i=1;i<=1000;i++){
+//     console.log(number , 'x' , i, '=',  i*3)
+// }
+
+
+// let number=2;
+// for(i=1;i<=10;i++){
+//     console.log(number ,  'x' , i , '=' , i*2)
+// }
+
+
+let number=4;
+for(i =1;i<=10;i++){
+    console.log(number , 'x' , i , '=' , i*4)
+}
--- while.js
+++ while.js
@@ -14,15 +14,63 @@
 // even number useing while loop
 
 
-let i =2;
-while(i<10){
-    console.log("The even number is " +i);
-    i=i+2;
-}
+// let i =2;
+// while(i<10){
+//     console.log("The even number is " +i);
+//     i=i+2;
+// }
 
 
-let count =1;
-while(count<20){
-    console.log("The odd number is " +count);
-    count=count+2;
+// let count =1;
+// while(count<20){
+//     console.log("The odd number is " +count);
+//     count=count+2;
+// }
+
+
+
+// for(let i=51; i<=70; i++){
+//     console.log("This is valeu is " +i)
+// }
+// for(let i=105; i<=159; i++){
+//     console.log("This is valeu is " +i)
+// }
+
+
+
+//  using is while loop
+
+
+// let i=51;
+// while(i<=70){
+//     console.log("This is valeue is " +i );
+//     i++;
+// }
+
+
+// let i =105;
+// while(i <=159){
+//     console.log("This is value is " +i);
+//     i++;
+// }
+// let sum=0;
+// for(let i =0; i<10;i++){
+//      sum=sum+i;
+//     console.log("Tottl value is " +sum)
+// }
+
+
+
+// for(let i =10;i>=0;i--){
+//     console.log("The decgariment value is " +i) 
+// }
+
+
+// for(let i =105;i>=0;i--){
+//     console.log(i)
+// }
+
+
+for(let i=107;i>=50;i--){
+    console.log(i)
 }
