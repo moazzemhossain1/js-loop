cd15b367da56ebc188ed5301761b9a9f7b9ebd42
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-18 08:27:55 +0800

    using breake and continue

---
+++ breake.js
@@ -0,0 +1,21 @@
+// for(let i=0;i<=10;i++){
+//     if(i>5){
+//         break;
+//     }
+//     console.log("The value is " , +i)
+// }
+
+// for(let i=0; i<=10;i++){
+//     if(i>5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+for(i=0;i<=15;i++){
+    if(i%2===1){
+        continue;
+    }
+    console.log("The odd number is ", +i)
+}
